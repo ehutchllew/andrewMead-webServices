c4a953294faf38525126bc05febbdb180f9d1363
Evan Hutchinson <evan.hutchinson@anthem.com>
2018-08-31 16:23:00 -0400

    added partials to the hbs templating, as well as some helpers

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ const hbs = require("hbs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
 app.get("/", (request, response) => {
-  // response.send('<h1>Hello Express!</h1>');
   response.render("home.hbs", {
     pageTitle: "Home Page",
-    message: "Welcome Home!",
-    currentYear: new Date().getFullYear()
+    message: "Welcome Home!"
   });
 });
 
 app.get("/about", (request, response) => {
   response.render("about.hbs", {
-    pageTitle: "About Page",
-    currentYear: new Date().getFullYear()
+    pageTitle: "About Page"
   });
 });
 
